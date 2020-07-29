3ad08fa861f1adffc58c2cebda71dc247bb702fa
Michele Barbiero <babishhh@gmail.com>
2020-07-29 19:37:25 +0000

    added lotsa comments explaining utility of most code

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 const net = require('net');
 
 const connect = function() {
+  // defines connection to server
   const conn = net.createConnection({ 
     host: 'localhost',
     port: 50541
@@ -8,19 +9,21 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  // event handler for messages received from server (f.e. message for being kicked when idle)
   conn.on('data', function(message) {
     console.log(message);
   });
 
+  // event handler when connecting to the server. sends server our username - (key Name, value MB). Also logs for the user that confirms joining the server
   conn.on('connect', () => {
     conn.write('Name: MB');
     console.log('Joined the server!');
-    // client.write('Hello there!');
   });
 
-  conn.on('connect', () => {
-    conn.write('Move: up');
-  });
+  // event handler that sends a message to the server. Server is set to handle specific strings as move inputs. this is not the ultimate movement input, just for testing
+  // conn.on('connect', () => {
+  //   conn.write('Move: up');
+  // });
   
   return conn;
 }
--- play.js
+++ play.js
@@ -1,12 +1,17 @@
 // const net = require('net');
 const { connect } = require('./client');
 
+// function that defines user inputs and settings:
 const setupInput = function() {
   const stdin = process.stdin;
+  // all inputs are considered without modifiers. not sure why this is necessary, but it's the reason why ctrl+c does not executes a SIGINT when in the program
   stdin.setRawMode(true);
+  // handles all node encoding in comprehensible text for us poor humans
   stdin.setEncoding('utf8');
+  // allows continuous input receiving. very nice
   stdin.resume();
 
+  // event that handles all valid key inputs. if key is not listed, program doesn't care
   stdin.on('data', (key) => {
     handleUserInput(key)
   })
@@ -25,12 +30,5 @@ setupInput();
 
 // logs message to user as soon as play is ran
 console.log('Connecting ...');
+// executes connect from client.js
 connect();
-
-
-/* 
-// handles standard input from the user. logs the key presses on the user terminal.
-process.stdin.on('data', function(message) {
-  client.write(message);
-});
-*/
